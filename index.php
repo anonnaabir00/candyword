747452745a89f802cfa0b9ea61b46f429f7929e6
asadabir <anonnaabir00@gmail.com>
2020-01-01 12:32:57 +0600

    - Turn off error reporting - Textarea added - Make it fully functional

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(0);
+error_reporting(0);
 require('main-functions.php');
 
 
@@ -10,17 +10,70 @@ $reading_time_calculator = new Candy_Reading_Time;
 //$reading_time_calculator->reading_time($_POST['wordcount']);
 
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title></title>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>Au Register Forms by Colorlib</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
 </head>
+
 <body>
-	<form action="" method="post">
-  		Input Text: <input type="text" size="100" name="wordcount" id="wordcount"><br>
-  	<input type="submit" value="Submit">
+<div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+<div class="wrapper wrapper--w680">
+<div class="card card-4">
+<div class="card-body">
+<img src="logo.png" class="logo">
+<h2 class="title">Total Words: <?php $count_all_words->word_counter($_POST['text_count']);?></h2>
+<form method="POST">
+<div class="row row-space">
+<div class="col-2">
+<div class="input-group">
+<label class="label">Enter Your Text Here:</label>
+<textarea class="input--style-4" rows="8" cols="57" name="text_count" id="text_count"></textarea>
+</div>
+</div>
+<div class="p-t-15">
+<button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
+</div>
 </form>
-<p>Total Word In this Sentence: <?php $count_all_words->word_counter($_POST['wordcount']);?></p>
-<p>Reading Time: <?php $reading_time_calculator->reading_time($_POST['wordcount']);?></p>
-</body>
+</div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>
+<!-- end document-->
